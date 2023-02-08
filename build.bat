910cc42865b57bb7ef914fcce71a8ed99ab93fea
Dylan Lucius <softwaretoy@gmail.com>
2023-02-07 22:30:58 -0800

    gbc v0.2.32 -- refactoring. changed bat file: erase .asm files during cleanup. also changed the 'song path issue' to

--- build.bat
+++ build.bat
@@ -9,8 +9,6 @@
 
 @set CVTFLAGS=-b0
 
-@echo Cleanup...
-
 @if exist %PROJ%.gb del %TGT%/%PROJ%.gb
 @if exist %PROJ%.sym del %TGT%/%PROJ%.map
 @if exist %PROJ%.sym del %TGT%/%PROJ%.ihx
@@ -18,18 +16,20 @@
 
 @if not exist %OBJ% mkdir %OBJ%
 
-@echo Assembling song and driver...
-
 tools\rgbasm -DGBDK -o%OBJ%/%DRV%.obj hUGEDriver.asm
 py tools\rgb2sdas.py %CVTFLAGS% -o %OBJ%\%DRV%.o %OBJ%\%DRV%.obj
 %GBDK%\bin\sdar -ru %OBJ%\hUGEDriver.lib %OBJ%\%DRV%.o
 
-@echo COMPILING WITH GBDK-2020...
-
 %GBDK%\bin\lcc -Wa-l -Wl-m -Wl-j -c -o Player.o Player.c
 %GBDK%\bin\lcc -Wa-l -Wl-m -Wl-j -c -o %PROJ%.o %PROJ%.c
-%GBDK%\bin\lcc -Wl-m -Wl-w -Wl-j -Wl-yp0x143=0x80 -Wm-yS -Wl-k%OBJ% -Wl-lhUGEDriver.lib -o %PROJ%.gb %PROJ%.c c:/users/admin/desktop/gbc-c192b9693f16fc57eccd76c7a1526f1e3b083aa4/song/C/%MOD%.c Player.o
+%GBDK%\bin\lcc -Wl-m -Wl-w -Wl-j -Wl-yp0x143=0x80 -Wm-yS -Wl-k%OBJ% -Wl-lhUGEDriver.lib -o gbc.gb %PROJ%.c c:/gbc/song/C/%MOD%.c Player.o
 
-del *.lst *.o *.sym *.map *.noi
+move hUGEDriver.asm obj
 
-@echo DONE!
+del *.lst *.o *.sym *.map *.noi *.asm
+
+cd obj
+
+move hUGEDriver.asm ..
+
+cd ..
